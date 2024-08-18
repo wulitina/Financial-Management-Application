92c002f43a23f0b419f46ac574d4d59ff1008c75
wulitina <tinamazing.yan.xu@gmail.com>
2024-08-18 12:35:35 +0100

    configure database, entity, and repository

---
+++ src/main/java/net/javaguides/banking/entity/Account.java
@@ -0,0 +1,23 @@
+package net.javaguides.banking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "accounts")
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "account_holder_name")
+    private String accountHolderName;
+    private double balance;
+
+}
